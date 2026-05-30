9057a00280defa58e8a0eca1e4239f29de2883cd
kinglukaing <kinglukainzy@gmail.com>
2026-05-30 22:15:29 +0000

    mplement initials printing function with stylized ASCII art output

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+ # File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Lukeman Nana Yaw Quansah
+# STUDENT ID:  2425402655
+# Email: 2425402655@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Lukeman Nana Yaw Quansah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+   
 
+    print()
+    print("...LNYQ")
+    print()
+    print("...LL.............NN........NN...YY........YY....QQQQQQQQQQ.")
+    print("...LL.............NNN.......NN...YY........YY...QQQ......QQQ")
+    print("...LL.............NNNN......NN....YY......YY....QQ........QQ")
+    print("...LL.............NN.NN.....NN.....YY....YY.....QQ........QQ")
+    print("...LL.............NN..NN....NN......YYYYYY......QQ........QQ")
+    print("...LL.............NN...NNN..NN.......YYYY.......QQ....QQ..QQ")
+    print("...LL.............NN.....NNNNN........YY........QQ.....QQQQQ")
+    print("...LLLLLLLLLL.....NN......NNNN........YY........QQQ....QQQQ.")
+    print("...LLLLLLLLLL**...NN.......NNN**......YY...**....QQQQQQQQQ**")
+    print("...LLLLLLLLLL**...NN........NN**......YY...**.....QQQQQQQQ**")
+    print()
 
 
 main()
